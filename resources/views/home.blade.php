13261119518c778df5524ba5c75f4a119969a074
jogjatanpakamu <aaamar.er@gmail.com>
2023-12-15 21:35:04 +0700

    tambah fitur hitung total aduan berdasarkan kategori aduan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,34 +84,7 @@
                     </div>
                 @endforeach
 
-                <!-- end counter -->
-                {{-- <!-- counter -->
-                <div class="col-md-3 col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated"
-                    data-wow-duration="600ms"
-                    style="visibility: visible; animation-duration: 600ms; animation-name: fadeInUp;">
-                    <i class="fa fa-heart medium-icon"></i>
-                    <span class="timer counter alt-font appear" data-to="980" data-speed="7000">980</span>
-                    <span class="counter-title">Happy Clients</span>
-                </div>
-                <!-- end counter -->
-                <!-- counter -->
-                <div class="col-md-3 col-sm-6 bottom-margin-small text-center counter-section wow fadeInUp xs-margin-bottom-ten animated"
-                    data-wow-duration="900ms"
-                    style="visibility: visible; animation-duration: 900ms; animation-name: fadeInUp;">
-                    <i class="fa fa-anchor medium-icon"></i>
-                    <span class="timer counter alt-font appear" data-to="810" data-speed="7000">810</span>
-                    <span class="counter-title">Projects Completed</span>
-                </div>
-                <!-- end counter -->
-                <!-- counter -->
-                <div class="col-md-3 col-sm-6 text-center counter-section wow fadeInUp animated"
-                    data-wow-duration="1200ms"
-                    style="visibility: visible; animation-duration: 1200ms; animation-name: fadeInUp;">
-                    <i class="fa fa-user medium-icon"></i>
-                    <span class="timer counter alt-font appear" data-to="600" data-speed="7000">600</span>
-                    <span class="counter-title">Clients Served</span>
-                </div>
-                <!-- end counter --> --}}
+
             </div>
         </div>
     </section>
